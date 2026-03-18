25f6ab2d0a0b20f197d72fb38cde17a9729028b9
tabithawanjikundungu@gmail.com <tabithawanjikundungu@gmail.com>
2026-03-19 01:18:56 +0300

    added main.tf

---
+++ DAY3/main.tf
@@ -0,0 +1,111 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform-user"
+}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/27"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "demo-public-subnet"
+  }
+}
+# Internet Gateway
+# ---------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "demo-main-igw"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+# Security Group
+# ---------------------------
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH, HTTP, HTTPS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c02fb55956c7d316" 
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  key_name = "mestock" 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Web Server is Running</h1><h2> This is my webserver for 30 days terraform challenge" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "web-server1"
+  }
+}
