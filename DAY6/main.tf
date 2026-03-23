d736a522c1b308156e29183f4c1c8b9e84fb09e8
Tabitha Ndungu <85933175+tabby123-dev@users.noreply.github.com>
2026-03-23 20:51:54 +0300

    Update main.tf

--- DAY6/main.tf
+++ DAY6/main.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-running-state-tabby123456"
+    bucket         = "terraform-running"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locks"
@@ -10,7 +10,7 @@ terraform {
   }
 }
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-running-state-tabby123456"
+  bucket = "terraform-running"
 
   lifecycle {
     prevent_destroy = true
