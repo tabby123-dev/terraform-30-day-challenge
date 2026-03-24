c9bad2650c7f6178b1ebe4f48daed417e8b1e019
Tabitha Ndungu <85933175+tabby123-dev@users.noreply.github.com>
2026-03-24 23:26:59 +0300

    Update main.tf

--- DAY8/live/dev/services/web_cluster/main.tf
+++ DAY8/live/dev/services/web_cluster/main.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket         = "terraform-running"
+    bucket         = "terraform-running-bucket"
     key            = "live/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
